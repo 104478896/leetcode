bf839ea8d0c5828d8dbe6a99a501869f6d114b16
104478896 <114716904+104478896@users.noreply.github.com>
2023-07-09 22:00:29 +0800

    Create 704. 二分查找.cpp

---
+++ 704. 二分查找.cpp
@@ -0,0 +1,25 @@
+/*给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/binary-search
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0;
+        int right=nums.size()-1;
+        int mid;
+        while(left<=right){
+            mid=(left+right)/2;
+            if(nums[mid]<target){
+                left=mid+1;
+            }else if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                return mid;
+            }
+            
+        }
+        return -1;
+    }
+};
