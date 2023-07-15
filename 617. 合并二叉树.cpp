b710af3449d631d4e96693c2c9ce958f9b86cba9
104478896 <114716904+104478896@users.noreply.github.com>
2023-07-15 16:58:15 +0800

    Create 617. 合并二叉树.cpp

---
+++ 617. 合并二叉树.cpp
@@ -0,0 +1,37 @@
+/*给你两棵二叉树： root1 和 root2 。
+
+想象一下，当你将其中一棵覆盖到另一棵之上时，两棵树上的一些节点将会重叠（而另一些不会）。你需要将这两棵树合并成一棵新二叉树。合并的规则是：如果两个节点重叠，那么将这两个节点的值相加作为合并后节点的新值；否则，不为 null 的节点将直接作为新二叉树的节点。
+
+返回合并后的二叉树。
+
+注意: 合并过程必须从两个树的根节点开始。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/merge-two-binary-trees
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1==NULL){
+            return root2;
+        }
+        if(root2==NULL){
+            return root1;
+        }
+        TreeNode* ans=new TreeNode(root1->val+root2->val);
+        ans->left=mergeTrees(root1->left,root2->left);
+        ans->right=mergeTrees(root1->right,root2->right);
+        return ans;
+    }
+};
