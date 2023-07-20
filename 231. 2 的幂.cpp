e856059731851ebbc2652d83f0ea67254b88cd19
104478896 <114716904+104478896@users.noreply.github.com>
2023-07-20 16:48:01 +0800

    Create 231. 2 的幂.cpp

---
+++ 231. 2 的幂.cpp
@@ -0,0 +1,34 @@
+/*给你一个整数 n，请你判断该整数是否是 2 的幂次方。如果是，返回 true ；否则，返回 false 。
+
+如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/power-of-two
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        // if(n<=0){
+        //     return false;
+        // }
+        // if(n==1){
+        //     return true;
+        // }
+        // while(n){
+        //     if(n==1){
+        //         return true;
+        //     }
+        //     if(n%2==1){
+        //         return false;
+        //     }
+        //     n/=2;
+        // }
+        // return true;
+        
+        if(n<=0){
+            return false;
+        }
+        return (n&(n-1))==0;
+
+    }
+};
