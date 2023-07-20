0ae5f8b9536fc7c6a98f0528b1fb20ed65b183c0
104478896 <114716904+104478896@users.noreply.github.com>
2023-07-20 16:47:48 +0800

    Create 1290. 二进制链表转整数.cpp

---
+++ 1290. 二进制链表转整数.cpp
@@ -0,0 +1,28 @@
+/*给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+
+请你返回该链表所表示数字的 十进制值 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/convert-binary-number-in-a-linked-list-to-integer
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int ans=0;
+        while(head){
+            ans=ans*2+head->val;
+            head=head->next;
+        }
+        return ans;
+    }
+};
