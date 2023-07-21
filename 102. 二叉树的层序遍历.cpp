ab8c7c4e2fcaf143ac14aadc979569e8bd0e92ca
104478896 <114716904+104478896@users.noreply.github.com>
2023-07-21 21:57:51 +0800

    Create 102. 二叉树的层序遍历.cpp

---
+++ 102. 二叉树的层序遍历.cpp
@@ -0,0 +1,40 @@
+/*给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(!root){
+            return ans;
+        }
+        queue<TreeNode*>que;
+        que.push(root);
+        while(!que.empty()){
+            int len=que.size();
+            vector<int>nums;
+            for(int i=0;i<len;++i){
+                TreeNode* temp=que.front();
+                que.pop();
+                nums.push_back(temp->val);
+                if(temp->left){
+                    que.push(temp->left);
+                }
+                if(temp->right){
+                    que.push(temp->right);
+                }
+            }
+            ans.push_back(nums);
+        }
+        return ans;
+    }
+};
