422c58a2184eab1641cd11a4541037b808a74574
104478896 <114716904+104478896@users.noreply.github.com>
2023-08-05 11:42:36 +0800

    Update 78. 子集.cpp

--- 78. 子集.cpp
+++ 78. 子集.cpp
@@ -3,18 +3,28 @@
 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。*/
 class Solution {
 public:
-    void dfs(vector<int>&nums,int index,vector<vector<int>>&ans,vector<int>&path){
-        if(index==nums.size()){
-            ans.push_back(path);
-            return ;
+    // //从输入角度回溯查找
+    // void dfs(vector<int>& nums,int index,vector<vector<int>>& ans,vector<int>&path){
+    //     if(index==nums.size()){
+    //         ans.push_back(path);
+    //         return;
+    //     }
+    //     dfs(nums,index+1,ans,path);
+    //     path.push_back(nums[index]);
+    //     dfs(nums,index+1,ans,path);
+    //     path.pop_back();
+    // }
+
+    //从输出即答案角度回溯查找
+    void dfs(vector<int>&nums,int index,vector<vector<int>>& ans,vector<int>&path){
+        ans.push_back(path);
+        for(int i=index;i<nums.size();++i){
+            path.push_back(nums[i]);
+            dfs(nums,i+1,ans,path);
+            path.pop_back();
         }
-        dfs(nums,index+1,ans,path);
-        path.push_back(nums[index]);
-        dfs(nums,index+1,ans,path);
-        path.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        int len=nums.size();
         vector<vector<int>>ans;
         vector<int>path;
         dfs(nums,0,ans,path);
